dbf8393237c2429e1e4fdfd20e5ac73c24b28db8
dris685 <56766548+dris685@users.noreply.github.com>
2022-07-07 01:18:42 -0400

    Update shortestWord.js

--- tests/05BONUS_shortestWord/shortestWord.js
+++ tests/05BONUS_shortestWord/shortestWord.js
@@ -1,5 +1,14 @@
-function findShortestWord() {
-
+function findShortestWord(str) {
+    strArray = []
+    strArray.push(str.split(" "))
+    newStrArray = strArray.flat()
+    minStr = newStrArray[0]
+    for(i = 0; i < newStrArray.length; i++){
+        if(newStrArray[i].length <= minStr.length){
+            minStr = newStrArray[i]
+        }
+    }
+    return minStr
 }
 
 module.exports = { findShortestWord };

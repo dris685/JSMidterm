52a19740193d8a37c5f238fb080a4c4d38a27cf0
dris685 <56766548+dris685@users.noreply.github.com>
2022-07-07 01:16:54 -0400

    Update betterThanAverage.test.js

--- tests/02_betterThanAverage.js/betterThanAverage.test.js
+++ tests/02_betterThanAverage.js/betterThanAverage.test.js
@@ -18,12 +18,12 @@ describe('betterThanAverage', () => {
     // Average should be rounded down to nearest whole number. checkout rounding methods to help.
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/floor
     const returnedValue = betterThanAverage([60, 70, 75, 90, 100], 90);
-    expect(returnedValue).to.equal('your score 90 is greater than class average 80');
+    expect(returnedValue).to.equal('your score 90 is greater than class average 79');
   });
 
   it('returns the correct string if your score is less than the average.', () => {
     const returnedValue = betterThanAverage([80, 85, 90, 90, 95, 100], 85);
-    expect(returnedValue).to.equal('your score 85 is lower than class average 89');
+    expect(returnedValue).to.equal('your score 85 is lower than class average 90');
   });
 
   it('returns the correct string if your score is equal to the average', () => {

b3109d47752e2c453e447f0780ab60416702c74a
dris685 <56766548+dris685@users.noreply.github.com>
2022-07-07 01:14:45 -0400

    Update betterThanAverage.js

--- tests/02_betterThanAverage.js/betterThanAverage.js
+++ tests/02_betterThanAverage.js/betterThanAverage.js
@@ -1,5 +1,17 @@
-function betterThanAverage() {
+function betterThanAverage(scoreArray = [], score) {
+    scoreTotal = 0
+    for(i = 0; i < scoreArray.length; i++){
+        scoreTotal = scoreTotal + scoreArray[i]
+    }
 
+    scoreAverage = Math.round(scoreTotal / scoreArray.length)
+    if(score > scoreAverage){
+       return `your score ${score} is greater than class average ${scoreAverage}`
+    }else if(score < scoreAverage){
+        return `your score ${score} is lower than class average ${scoreAverage}`
+    }else{
+        return `your score ${score} is equal to class average ${scoreAverage}`
+    }
 }
 
 module.exports = { betterThanAverage };

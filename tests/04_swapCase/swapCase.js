3864aa215f5b049362e4af0647e68a15cf5965e0
dris685 <56766548+dris685@users.noreply.github.com>
2022-07-07 01:18:13 -0400

    Update swapCase.js

--- tests/04_swapCase/swapCase.js
+++ tests/04_swapCase/swapCase.js
@@ -1,5 +1,15 @@
-function swapCase() {
-
+function swapCase(str) {
+    newStr = ""
+    for(i = 0; i < str.length; i++){
+        if(str.charAt(i) === str.charAt(i).toUpperCase()){
+            newStr = newStr + str.charAt(i).toLowerCase()
+        }else if(str.charAt(i) === str.charAt(i).toLowerCase()){
+            newStr = newStr + str.charAt(i).toUpperCase()
+        }else{
+            newStr = newStr + str.charAt(i)
+        }
+    }
+    return newStr
 }
 
 module.exports = { swapCase };

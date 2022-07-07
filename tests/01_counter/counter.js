b0cab87b7674a0d76b5a50dfc0d8f883835f8b32
dris685 <56766548+dris685@users.noreply.github.com>
2022-07-07 01:14:08 -0400

    Update counter.js

--- tests/01_counter/counter.js
+++ tests/01_counter/counter.js
@@ -1,5 +1,9 @@
-function counter() {
-
+function counter(num) {
+    counterArray = []
+    for(i = 1; i <= num; i++){
+        counterArray.push(i)
+    }
+    return counterArray
 }
 
 module.exports = { counter };

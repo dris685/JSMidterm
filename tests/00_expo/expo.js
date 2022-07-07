917cf2811aa2e0b8e56ec0911493dfaf123602cc
dris685 <56766548+dris685@users.noreply.github.com>
2022-07-07 01:13:33 -0400

    Update expo.js

--- tests/00_expo/expo.js
+++ tests/00_expo/expo.js
@@ -1,6 +1,11 @@
 // Do not use Math.pow() in your solution.
-function expo() {
-
+function expo(num1 = 1, num2 = 0) {
+    if (num1 != 0){
+        powerNum = num1 ** num2
+    }else{
+        powerNum = "num1 cannot be 0"
+    }
+    return powerNum
 }
 
 module.exports = { expo };
